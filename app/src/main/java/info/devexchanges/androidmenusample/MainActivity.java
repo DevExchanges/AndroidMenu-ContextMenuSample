edb7e84ca810cb1b761fdf155efbe242f8f06a80
ecpay <ecpay@ecpays-MacBook-Pro.local>
2015-11-25 23:56:24 +0700

    push project

---
+++ app/src/main/java/info/devexchanges/androidmenusample/MainActivity.java
@@ -0,0 +1,183 @@
+package info.devexchanges.androidmenusample;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> countries;
+    private ArrayAdapter<String> adapter;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        addingDataToList();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, countries);
+        listView.setAdapter(adapter);
+
+        setSupportActionBar(toolbar);
+
+        //register contextMenu with TextView
+        registerForContextMenu(listView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.a_z) {
+            sortByName(countries);
+            return true;
+
+        } else if (item.getItemId() == R.id.z_a) {
+            reverseByName(countries);
+            return true;
+
+        } else if (item.getItemId() == R.id.add) {
+            showAddingDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context_main, menu);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        menu.setHeaderTitle(countries.get(info.position));
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        //find out which menu item was pressed
+        switch (item.getItemId()) {
+            case R.id.delete:
+                countries.remove(info.position);
+                adapter.notifyDataSetChanged();
+                return true;
+            case R.id.go_wiki:
+                String listItemName = countries.get(info.position);
+                openBrowser("https://en.wikipedia.org/wiki/" + listItemName);
+
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * sort countries by name A - Z
+     */
+    private void sortByName(ArrayList<String> countries) {
+        Collections.sort(countries, new Comparator<String>() {
+            public int compare(String v1, String v2) {
+                return v1.compareTo(v2);
+            }
+        });
+    }
+
+    /**
+     * sort countries by name Z - A
+     */
+    private void reverseByName(ArrayList<String> countries) {
+        Collections.sort(countries, Collections.reverseOrder(new Comparator<String>() {
+
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        }));
+    }
+
+    private void showAddingDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.layout_add_dialog, null, false);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editText = (EditText) dialogView.findViewById(R.id.edit_text);
+
+        dialogBuilder.setTitle("Adding new Country");
+        dialogBuilder.setMessage("Input a Country name");
+
+        dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if (editText.getText().toString().trim().equals("")) {
+                    Toast.makeText(MainActivity.this, "Please input some texts!", Toast.LENGTH_SHORT).show();
+                } else {
+                    //add new country name to List
+                    countries.add(editText.getText().toString().trim());
+
+                    //update ListView adapter
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this, "Adding sucessful!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //do nothing, just close this dialog
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+    private void openBrowser(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+
+        startActivity(i);
+    }
+
+    private void addingDataToList() {
+        countries = new ArrayList<>();
+        countries.add("Vietnam");
+        countries.add("Japan");
+        countries.add("China");
+        countries.add("Singapore");
+        countries.add("USA");
+        countries.add("Russia");
+        countries.add("Korea");
+        countries.add("Thailand");
+        countries.add("India");
+        countries.add("Malaysia");
+        countries.add("Philippines");
+        countries.add("Germany");
+        countries.add("France");
+        countries.add("Netherlands");
+    }
+}
